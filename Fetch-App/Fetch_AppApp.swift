b7318f1f7d25c79a1996aa72e1bcfba62d0efd10
Ignacio Juarez <62676603+ignaciojuarez@users.noreply.github.com>
2024-07-22 19:09:37 -0600

    Initial Commit

---
+++ Fetch-App/Fetch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_AppApp.swift
+//  Fetch-App
+//
+//  Created by Ignacio Juarez on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
